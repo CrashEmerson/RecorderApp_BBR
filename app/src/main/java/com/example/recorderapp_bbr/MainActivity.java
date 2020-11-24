79f0fc1d4f7da15dbe9faea93baca364cc54de46
Emerson <emersoncrash256@gmail.com>
2020-11-24 17:19:55 +0530

    Initial commit

---
+++ app/src/main/java/com/example/recorderapp_bbr/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.recorderapp_bbr;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
